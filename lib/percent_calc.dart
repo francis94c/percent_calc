446df9f047304cb4fae3a681ff53ffc5899ff26f
Francis Ilechukwu <francis94c@gmail.com>
2021-12-08 20:54:12 +0100

    Done.

--- lib/percent_calc.dart
+++ lib/percent_calc.dart
@@ -1,8 +1,18 @@
 library percent_calc;
 
 /// Percent Calc
-class PercentCalc {
+extension PercentCalc on double {
   /// Gets the actual change in value based on a given percentage.
-  static double valueChange(double currentVal, double percentageChange) =>
-      currentVal * (100 / (percentageChange + 100));
+  double valueChange(double percentageChange) =>
+      this - (this * (100 / (percentageChange + 100)));
+
+  /// Get the original value of the number prior to the change.
+  double originalValue(double percentageChange) =>
+      this * (100 / (percentageChange + 100));
+
+  /// Get value of percentage
+  double percentValue(double percentage) => (percentage / 100) * this;
+
+  /// Get percentage difference.
+  double percentDiff(double newValue) => ((newValue - this) / this) * 100;
 }
--- test/percent_calc_test.dart
+++ test/percent_calc_test.dart
@@ -3,10 +3,38 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:percent_calc/percent_calc.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('Value Change +', () {
+    double value = 294;
+    expect(value.valueChange(5), 14);
+  });
+
+  test('Value Change -', () {
+    double value = 266;
+    expect(value.valueChange(-5), -14);
+  });
+
+  test('Original Value +', () {
+    double value = 294;
+    expect(value.originalValue(5), 280);
+  });
+
+  test('Original Value -', () {
+    double value = 266;
+    expect(value.originalValue(-5), 280);
+  });
+
+  test('Percentage value of', () {
+    double value = 200;
+    expect(value.percentValue(10), 20);
+  });
+
+  test('Percentage difference -', () {
+    double value = 280;
+    expect(value.percentDiff(266), -5);
+  });
+
+  test('Percentage difference +', () {
+    double value = 280;
+    expect(value.percentDiff(294), 5);
   });
 }
